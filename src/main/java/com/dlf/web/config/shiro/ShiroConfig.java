ed32fd27d91071f587d3458f6096f51fa160e1bf
Someone <oncwnuOIzH_4tT_FuTFpX7g3Kj3U@git.weixin.qq.com>
2020-06-16 14:52:37 +0800

    ～～～

--- src/main/java/com/dlf/web/config/shiro/ShiroConfig.java
+++ src/main/java/com/dlf/web/config/shiro/ShiroConfig.java
@@ -45,6 +45,7 @@ public class ShiroConfig {
         //配置退出 过滤器,其中的具体的退出代码Shiro已经替我们实现了，登出后跳转配置的loginUrl
         // 配置不会被拦截的链接 顺序判断
         filterChainDefinitionMap.put("/login", "anon");
+        filterChainDefinitionMap.put("/logout", "anon");
         filterChainDefinitionMap.put("/register", "anon");
         filterChainDefinitionMap.put("/wxsp/register", "anon");
         filterChainDefinitionMap.put("/wxsp/checkUser", "anon");
